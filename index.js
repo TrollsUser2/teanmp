f7f4c6958ea2eb1a518bfc3bf992f7d47c5da072
trollsuser <100897115+TrollsUser2@users.noreply.github.com>
2024-03-04 13:45:39 +0700

    index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const count = require ('text-count');
+function 0xsatsss() {
+  conts text = "0 x S A T S S S"
+  return {
+    text,
+    length: count.charCount(text)
+  }
+}
+module.exports - 0xsatsss
